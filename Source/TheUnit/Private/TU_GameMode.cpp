c7cece6b2a9fbc11fc1425f5141bc437aa4e6ed2
MAVet710 <149276571+MAVet710@users.noreply.github.com>
2026-09-08 18:13:27 -0400

    Use integrated modular armed operator as default pawn

--- Source/TheUnit/Private/TU_GameMode.cpp
+++ Source/TheUnit/Private/TU_GameMode.cpp
@@ -1,8 +1,10 @@
 #include "TU_GameMode.h"
 
+#include "TU_ModularOperatorCharacter.h"
 #include "TU_PlayerController.h"
 
 ATU_GameMode::ATU_GameMode()
 {
+    DefaultPawnClass = ATU_ModularOperatorCharacter::StaticClass();
     PlayerControllerClass = ATU_PlayerController::StaticClass();
 }
