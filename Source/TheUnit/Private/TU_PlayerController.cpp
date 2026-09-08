20d04b29c922ed7d17f70e1d1115f667222d1910
MAVet710 <149276571+MAVet710@users.noreply.github.com>
2026-09-07 20:08:29 -0400

    Bind field MX50 toggle in player controller

--- Source/TheUnit/Private/TU_PlayerController.cpp
+++ Source/TheUnit/Private/TU_PlayerController.cpp
@@ -1,1 +1,36 @@
 #include "TU_PlayerController.h"
+
+#include "TU_ArmedOperatorCharacter.h"
+#include "Components/InputComponent.h"
+
+void ATU_PlayerController::SetupInputComponent()
+{
+    Super::SetupInputComponent();
+
+    if (InputComponent)
+    {
+        InputComponent->BindAction(TEXT("ToggleMX50"), IE_Pressed, this, &ATU_PlayerController::ToggleMX50);
+    }
+}
+
+void ATU_PlayerController::ToggleMX50()
+{
+    ATU_ArmedOperatorCharacter* Operator = Cast<ATU_ArmedOperatorCharacter>(GetPawn());
+    if (!Operator)
+    {
+        return;
+    }
+
+    if (Operator->IsBriefingOpen())
+    {
+        Operator->CloseBriefing();
+        return;
+    }
+
+    if (Operator->IsArmoryOpen() || Operator->IsMeleeEquipped())
+    {
+        return;
+    }
+
+    Operator->OpenBriefing(NAME_None, FText());
+}
