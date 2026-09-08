26ca8ef9a50515d2238a2ba26da03ccda609dd85
MAVet710 <149276571+MAVet710@users.noreply.github.com>
2026-09-07 20:09:45 -0400

    Persist and reuse MX50 state across field access

--- Source/TheUnit/Private/TU_PlayerController.cpp
+++ Source/TheUnit/Private/TU_PlayerController.cpp
@@ -1,7 +1,13 @@
 #include "TU_PlayerController.h"
 
 #include "TU_ArmedOperatorCharacter.h"
+#include "TUMX50TabletComponent.h"
 #include "Components/InputComponent.h"
+
+ATU_PlayerController::ATU_PlayerController()
+{
+    MX50Tablet = CreateDefaultSubobject<UTUMX50TabletComponent>(TEXT("MX50Tablet"));
+}
 
 void ATU_PlayerController::SetupInputComponent()
 {
@@ -32,5 +38,13 @@ void ATU_PlayerController::ToggleMX50()
         return;
     }
 
-    Operator->OpenBriefing(NAME_None, FText());
+    if (MX50Tablet)
+    {
+        const FTMX50MissionSnapshot Snapshot = MX50Tablet->GetMissionSnapshot();
+        Operator->OpenBriefing(Snapshot.MissionId, Snapshot.MissionTitle);
+    }
+    else
+    {
+        Operator->OpenBriefing(NAME_None, FText());
+    }
 }
