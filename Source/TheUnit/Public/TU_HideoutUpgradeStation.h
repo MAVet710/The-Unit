d9c9412608ecce4478876433e55ce3444995a39f
MAVet710 <149276571+MAVet710@users.noreply.github.com>
2026-09-08 18:24:39 -0400

    Add in-world status display to physical hideout upgrade stations

--- Source/TheUnit/Public/TU_HideoutUpgradeStation.h
+++ Source/TheUnit/Public/TU_HideoutUpgradeStation.h
@@ -5,6 +5,9 @@
 #include "TUHideoutProgressionComponent.h"
 #include "TU_HideoutUpgradeStation.generated.h"
 
+class UTextRenderComponent;
+class UTUHideoutProgressionComponent;
+
 /** Physical interaction point for one upgradeable hideout module. */
 UCLASS(Blueprintable)
 class THEUNIT_API ATU_HideoutUpgradeStation : public ATU_CommandCenterStation
@@ -13,6 +16,7 @@ class THEUNIT_API ATU_HideoutUpgradeStation : public ATU_CommandCenterStation
 
 public:
     ATU_HideoutUpgradeStation();
+    virtual void BeginPlay() override;
 
     UFUNCTION(BlueprintCallable, Category="Hideout|Upgrade")
     void ConfigureUpgradeStation(ETUHideoutModuleType InModuleType, const FText& InLabel);
@@ -20,9 +24,18 @@ public:
     UFUNCTION(BlueprintPure, Category="Hideout|Upgrade")
     ETUHideoutModuleType GetModuleType() const { return ModuleType; }
 
+    UFUNCTION(BlueprintCallable, Category="Hideout|Upgrade")
+    void RefreshStatus();
+
     virtual bool UseStation(ATU_ArmedOperatorCharacter* Operator) override;
 
 protected:
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Hideout|Upgrade")
     ETUHideoutModuleType ModuleType = ETUHideoutModuleType::Power;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Hideout|Upgrade")
+    TObjectPtr<UTextRenderComponent> StatusText;
+
+private:
+    UTUHideoutProgressionComponent* FindProgression() const;
 };
