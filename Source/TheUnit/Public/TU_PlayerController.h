9a4396a76bac4d2241d9e179470ae89c5fec90bd
MAVet710 <149276571+MAVet710@users.noreply.github.com>
2026-09-07 20:09:32 -0400

    Persist MX50 tablet state on player controller

--- Source/TheUnit/Public/TU_PlayerController.h
+++ Source/TheUnit/Public/TU_PlayerController.h
@@ -4,14 +4,25 @@
 #include "GameFramework/PlayerController.h"
 #include "TU_PlayerController.generated.h"
 
+class UTUMX50TabletComponent;
+
 /** Player input and local-control bridge for tactical UI commands. */
 UCLASS()
 class THEUNIT_API ATU_PlayerController : public APlayerController
 {
     GENERATED_BODY()
 
+public:
+    ATU_PlayerController();
+
+    UFUNCTION(BlueprintPure, Category="MX50")
+    UTUMX50TabletComponent* GetMX50Tablet() const { return MX50Tablet; }
+
 protected:
     virtual void SetupInputComponent() override;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="MX50")
+    TObjectPtr<UTUMX50TabletComponent> MX50Tablet;
 
 private:
     void ToggleMX50();
